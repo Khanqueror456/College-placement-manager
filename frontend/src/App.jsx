0633dc64b9130548a94625e96a72cf0f7c89f3af
Md Saad Khan <unrealengine456@gmail.com>
2025-11-07 22:21:01 +0530

    Add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,20 @@ import StudentProfile from './pages/student_profile';
 import EditStudentProfile from './pages/EditStudentProfile';
 import StudentDrives from './pages/StudentDrives';
 import StudentDriveStatus from './pages/StudentDriveStatus';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <StudentDriveStatus />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/hod-dashboard" element={<HodDashboard />} />
+        
+        {/* This is the "catch-all" route for 404s */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
         
     </>
   )
